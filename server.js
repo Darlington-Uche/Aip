2694b8855fb994b3c9dc870ed27c316a2f4c702f
Darlington <darlingtonweb3@gmail.com>
2025-08-12 22:11:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // Firebase server using Express and Firestor
 
-
-
 const express = require('express');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
@@ -10,8 +8,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Firebase Admin SDK setup
-const serviceAccount = require('./fire.json');
+require('dotenv').config();
 
+const serviceAccount = {
+  type: process.env.FIRE_TYPE,
+  project_id: process.env.FIRE_PROJECT_ID,
+  private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
+  private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIRE_CLIENT_EMAIL,
+  client_id: process.env.FIRE_CLIENT_ID,
+  auth_uri: process.env.FIRE_AUTH_URI,
+  token_uri: process.env.FIRE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIRE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIRE_UNIVERSE_DOMAIN
+};
+module.exports = serviceAccount;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
